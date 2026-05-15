5d929f6e089f33e1e393e3697cad98367dc5da3b
Bruno <brunoriquelme.coutinho@gmail.com>
2026-05-14 21:55:16 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-temperaturas = [
+temperatura = [
     [28, 31, 34, 33],
     [25, 27, 29, 28],
     [32, 35, 36, 34],
     [24, 26, 25, 27]
 ]
 
-maior_criticos = 0
-sala_maior_risco = 0
+maior = 0
+sala = 0
 
-for i in range(len(temperaturas)):
+for i in range(len(temperatura)):
     soma = 0
     criticos = 0
 
-    for temp in temperaturas[i]:
+    for temp in temperatura[i]:
         soma += temp
         if temp >= 33:
             criticos += 1
 
-    media = soma / len(temperaturas[i])
+    media = soma / len(temperatura[i])
 
     print(f"Sala {i + 1} \n Média: {media} \n Registros críticos: {criticos}")
 
-    if criticos > maior_criticos:
-        maior_criticos = criticos
-        sala_maior_risco = i + 1
+    if criticos > maior:
+        maior = criticos
+        sala = i + 1
 
-print(f"\nSala com maior risco: Sala {sala_maior_risco}")
+print()
+print(f"Sala com maior risco: Sala {sala}")
